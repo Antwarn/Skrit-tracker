28535d245f2f7a26ce4097ce82e7cc0c2c85f412
Anton Merkelsen <anton.v.merkelsen@gmail.com>
2023-09-24 11:59:01 +0200

    Opdateret version

--- script.js
+++ script.js
@@ -1,44 +1,74 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const yourProgressBar = document.getElementById("your-progress-bar");
-    const yourStepCountElement = document.getElementById("your-step-count");
-    const friendProgressBar = document.getElementById("friend-progress-bar");
-    const friendStepCountElement = document.getElementById("friend-step-count");
-    const addButton = document.getElementById("add-step-btn");
-    const addFriendButton = document.getElementById("add-friend-step-btn");
+document.addEventListener('DOMContentLoaded', function () {
+    const stepsElement1 = document.getElementById('steps1');
+    const stepsElement2 = document.getElementById('steps2');
+    const incrementButton1 = document.getElementById('increment1');
+    const incrementButton2 = document.getElementById('increment2');
+    const progress1 = document.getElementById('progress1');
+    const progress2 = document.getElementById('progress2');
+    const friendList = document.getElementById('friend-list');
+    const createTaskButton = document.getElementById('createTask');
+    const taskNameInput = document.getElementById('taskName');
 
-    let yourStepCount = 0;
-    let friendStepCount = 0;
-    const goalSteps = 50;
+    let steps1 = 0;
+    let steps2 = 0;
+    const friends = [{ name: "Friend 1", steps: 0 }, { name: "Friend 2", steps: 0 }];
+    const goal = 100;
+    let loser = null;
+    let currentTaskName = "";
 
-    updateProgressBar();
-
-    addButton.addEventListener("click", function () {
-        yourStepCount++;
-        updateProgressBar();
+    incrementButton1.addEventListener('click', () => {
+        steps1++;
+        stepsElement1.innerText = steps1;
+        updateFriendSteps("Friend 1", steps1);
+        updateProgress(progress1, steps1);
+        checkLoser("Friend 1", steps1);
     });
 
-    addFriendButton.addEventListener("click", function () {
-        friendStepCount++;
-        updateProgressBar();
+    incrementButton2.addEventListener('click', () => {
+        steps2++;
+        stepsElement2.innerText = steps2;
+        updateFriendSteps("Friend 2", steps2);
+        updateProgress(progress2, steps2);
+        checkLoser("Friend 2", steps2);
     });
 
-    function updateProgressBar() {
-        const yourProgress = (yourStepCount / goalSteps) * 100;
-        yourProgressBar.style.width = yourProgress + "%";
-        yourStepCountElement.textContent = yourStepCount + " steps";
-
-        const friendProgress = (friendStepCount / goalSteps) * 100;
-        friendProgressBar.style.width = friendProgress + "%";
-        friendStepCountElement.textContent = friendStepCount + " steps";
-
-        if (yourStepCount >= goalSteps) {
-            yourStepCountElement.textContent = "Tillykke du har vundet over David!!";
-            addButton.disabled = true;
+    createTaskButton.addEventListener('click', () => {
+        const taskName = taskNameInput.value.trim();
+        if (taskName !== '') {
+            currentTaskName = taskName;
+            friends.push({ name: taskName, steps: 0 });
+            taskNameInput.value = '';
+            renderFriends();
         }
+    });
 
-        if (friendStepCount >= goalSteps) {
-            friendStepCountElement.textContent = "David har vundet over dig, du er en taber!";
-            addFriendButton.disabled = true;
+    function updateFriendSteps(name, steps) {
+        const friendIndex = friends.findIndex((friend) => friend.name === name);
+        if (friendIndex !== -1) {
+            friends[friendIndex].steps = steps;
+            renderFriends();
         }
+    }
+
+    function updateProgress(progress, steps) {
+        progress.value = steps;
+        // Update the progress bar label to show the step count
+        progress.innerHTML = `${steps} / ${goal} steps`;
+    }
+
+    function checkLoser(name, steps) {
+        if (steps >= goal && (loser === null || steps > friends.find(f => f.name === loser).steps)) {
+            loser = name;
+            alert(`${loser} is the loser and must do the task: ${currentTaskName}`);
+        }
+    }
+
+    function renderFriends() {
+        friendList.innerHTML = '';
+        friends.forEach((friend) => {
+            const listItem = document.createElement('li');
+            listItem.innerText = `${friend.name}: ${friend.steps} steps`;
+            friendList.appendChild(listItem);
+        });
     }
 });
